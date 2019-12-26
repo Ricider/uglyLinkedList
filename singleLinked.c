9a771b71fcc15bbbeae5cf4230a521cda724e9c8
Ricider <54453420+Ricider@users.noreply.github.com>
2019-12-25 21:10:35 -0500

    Add files via upload

--- singleLinked.c
+++ singleLinked.c
@@ -11,15 +11,16 @@ typedef struct _HeadPtr{
 	struct _LinkedList* first;
 	
 	//method pointers
-	void (*add) (struct _HeadPtr*,int);
+	void (*add) (struct _HeadPtr,int);
 	int (*delete) (struct _HeadPtr*,int);
-	int (*get) (struct _HeadPtr*,int);
-	void (*addAfter) (struct _HeadPtr*,int,int);
+	int (*get) (struct _HeadPtr,int);
+	void (*addAfter) (struct _HeadPtr,int,int);
+	void (*print) (struct _HeadPtr);
 }List;
 
 //methods
-void regularAdd(List* this,int newElement){
-	Node* current=this->first;
+void regularAdd(List this,int newElement){
+	Node* current=this.first;
 	while (current->next!=0){
 		current=current->next;
 	}
@@ -27,8 +28,8 @@ void regularAdd(List* this,int newElement){
 	current->next->element=newElement;
 }
 
-int regularGet(List* this,int index){
-	Node* current=this->first;
+int regularGet(List this,int index){
+	Node* current=this.first;
 	for (;index>0;index--){
 		current=current->next;
 	}
@@ -56,8 +57,8 @@ int regularDelete(List* this,int index){
 	return returnInt;
 }
 
-void regularAddAfter(List* this,int index,int newElement){
-	Node* current=this->first;
+void regularAddAfter(List this,int index,int newElement){
+	Node* current=this.first;
 	for (;index>0;index--){
 		current=current->next;
 	}
@@ -67,8 +68,18 @@ void regularAddAfter(List* this,int index,int newElement){
 	current->next->next=previousNext;
 }
 
+void regularPrint(List this){
+	Node* current=this.first;
+	printf("[ ");
+	do {
+		printf("%d ",current->element);
+		current=current->next;
+	} while (current!=0);
+	printf("]\n");
+}
+
 //RegularList constructor
-List* new_RegularList(int firstElement){
+List new_RegularList(int firstElement){
 	List* newList=(List*) malloc(sizeof (List));
 	newList->first=(Node*) malloc(sizeof (Node));
 	newList->first->element=firstElement;
@@ -76,19 +87,25 @@ List* new_RegularList(int firstElement){
 	newList->get=regularGet;
 	newList->delete=regularDelete;
 	newList->addAfter=regularAddAfter;
+	newList->print=regularPrint;
 	
-	return newList;
+	return *newList;
 }
 
 int main(){
-	List* testList=new_RegularList(4);
-	(*testList).add(testList,5);
-	(*testList).add(testList,6);
-	(*testList).add(testList,7);
-	printf("%d\n",(*testList).delete(testList,1));
-	(*testList).addAfter(testList,0,8);
-	printf("%d\n",(*testList).delete(testList,0));
-	printf("%d  %d  %d\n",(*testList).get(testList,0),(*testList).get(testList,1),(*testList).get(testList,2));
+	List testList=new_RegularList(4);
+	testList.add(testList,5);
+	testList.add(testList,6);
+	testList.add(testList,7);
+	testList.print(testList);
+	printf("removed %d from pos 1\n",testList.delete(&testList,1));
+	testList.addAfter(testList,0,8);
+	printf("added 8 after pos 0\n");
+	testList.print(testList);
+	printf("removed %d from pos 0\n",testList.delete(&testList,0));
+	testList.print(testList);
+	//printf("%d\n",testList.first->next);
+	printf("first element is %d\n",testList.get(testList,0));
 	return 0;
 }
 
